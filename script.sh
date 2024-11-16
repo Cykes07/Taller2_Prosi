3689af7fb843d3326f1a7a710c081fa57630409b
Bryan Zhang <140031745+bgzhangg@users.noreply.github.com>
2024-11-15 20:24:53 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Función para verificar si la ubicación ingresada es válida
 function verificar_ubicacion() {
     while true; do
         if [ -d "$1" ]; then
@@ -14,23 +13,17 @@ function verificar_ubicacion() {
     done
 }
 
-# Solicitar ubicación al usuario
 read -p "Ingresa la ubicación: " ubicacion
 verificar_ubicacion "$ubicacion"
 
-# Cambiar a la ubicación indicada
 cd "$ubicacion" || exit
 
-# Crear archivo original.txt con listado inicial
 find . -exec ls -ld {} \; > original.txt
 
-# Cambiar permisos para los archivos en el directorio actual
 find . -maxdepth 1 -type f -exec chmod 743 {} \;
 
-# Cambiar permisos para los archivos en los subdirectorios
 find . -mindepth 2 -type f -exec chmod 542 {} \;
 
-# Crear archivo final.txt con listado modificado
 find . -exec ls -ld {} \; > final.txt
 
 echo "Proceso completado. Los resultados están en original.txt y final.txt."
